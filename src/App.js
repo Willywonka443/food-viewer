4e54339e8e9b766e8e7b6be64cb11dda02163251
Willywonka443 <spaceiskool1998@gmail.com>
2022-09-16 13:50:59 -0400

    Birthday

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <SaveIcon />
-        <Typography>
+        <Typography variant="h4">
           William Rocks!
         </Typography>
         <a
