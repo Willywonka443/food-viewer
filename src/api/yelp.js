536472c5fd87a01d735ecf45ccf50c91f301d32c
Willywonka443 <spaceiskool1998@gmail.com>
2022-10-03 14:24:26 -0400

    Final Change for test

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const async (location, term) => {
+const Axios = async (location, term) => {
    const response = await axios({
         method : 'get',
         url : `https://yelp-backend.netlify.app/.netlify/functions/search?location=${location}&term=${term}`
